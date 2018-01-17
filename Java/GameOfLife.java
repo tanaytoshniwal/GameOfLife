c9938e8c2e7ed90d747948ce551aa8a5c22cb6df
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-01-17 16:40:34 +0530

    glider gun

--- Java/GameOfLife.java
+++ Java/GameOfLife.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -7,6 +9,8 @@ import javax.swing.JPanel;
 
 public class GameOfLife extends JFrame{
 	static JButton cell[][] = new JButton[69][69];
+	JButton start,stop,reset;
+	static int grid[][] = new int[69][69];
 	static JPanel pan;
 	static GameOfLife frame = null;
 	static boolean b = true;
@@ -18,127 +22,162 @@ public class GameOfLife extends JFrame{
 		pan = new JPanel(new GridLayout(69,69));
 		for(int i=0;i<69;i++) {
 			for(int j=0;j<69;j++) {
+				grid[i][j]=0;
 				cell[i][j] = new JButton();
 				cell[i][j].setBackground(Color.WHITE);
 				cell[i][j].setVisible(true);
-				cell[i][j].setBorder(null);
+				//cell[i][j].setBorder(null);
 				cell[i][j].setEnabled(false);
-				set(i,j);
 				pan.add(cell[i][j]);
+				if(i==0||j==0||i==68||j==68)
+					cell[i][j].setBackground(Color.RED);
 			}
 		}
+		start=new JButton("START");
+		stop=new JButton("STOP");
+		reset=new JButton("reset");
+		set();
 		add(pan);
 	}
-	void set(int i, int j) {
-		if(i==0 && j==4)
+	void set() {
+		cell[6][2].setBackground(Color.BLACK);
+		cell[6][3].setBackground(Color.BLACK);
+		cell[7][2].setBackground(Color.BLACK);
+		cell[7][3].setBackground(Color.BLACK);
+		cell[6][12].setBackground(Color.BLACK);
+		cell[7][12].setBackground(Color.BLACK);
+		cell[8][12].setBackground(Color.BLACK);
+		cell[5][13].setBackground(Color.BLACK);
+		cell[9][13].setBackground(Color.BLACK);
+		cell[4][14].setBackground(Color.BLACK);
+		cell[4][15].setBackground(Color.BLACK);
+		cell[10][14].setBackground(Color.BLACK);
+		cell[10][15].setBackground(Color.BLACK);
+		cell[7][16].setBackground(Color.BLACK);
+		cell[5][17].setBackground(Color.BLACK);
+		cell[9][17].setBackground(Color.BLACK);
+		cell[6][18].setBackground(Color.BLACK);
+		cell[7][18].setBackground(Color.BLACK);
+		cell[8][18].setBackground(Color.BLACK);
+		cell[7][19].setBackground(Color.BLACK);
+		cell[6][22].setBackground(Color.BLACK);
+		cell[5][22].setBackground(Color.BLACK);
+		cell[4][22].setBackground(Color.BLACK);
+		cell[4][23].setBackground(Color.BLACK);
+		cell[5][23].setBackground(Color.BLACK);
+		cell[6][23].setBackground(Color.BLACK);
+		cell[3][24].setBackground(Color.BLACK);
+		cell[7][24].setBackground(Color.BLACK);
+		cell[3][26].setBackground(Color.BLACK);
+		cell[7][26].setBackground(Color.BLACK);
+		cell[2][26].setBackground(Color.BLACK);
+		cell[8][26].setBackground(Color.BLACK);
+		cell[4][36].setBackground(Color.BLACK);
+		cell[4][37].setBackground(Color.BLACK);
+		cell[5][36].setBackground(Color.BLACK);
+		cell[5][37].setBackground(Color.BLACK);
+		
+
+		grid[6][2]=1;
+		grid[6][3]=1;
+		grid[7][2]=1;
+		grid[7][3]=1;
+		grid[6][12]=1;
+		grid[7][12]=1;
+		grid[8][12]=1;
+		grid[5][13]=1;
+		grid[9][13]=1;
+		grid[4][14]=1;
+		grid[4][15]=1;
+		grid[10][14]=1;
+		grid[10][15]=1;
+		grid[7][16]=1;
+		grid[5][17]=1;
+		grid[9][17]=1;
+		grid[6][18]=1;
+		grid[7][18]=1;
+		grid[8][18]=1;
+		grid[7][19]=1;
+		grid[6][22]=1;
+		grid[5][22]=1;
+		grid[4][22]=1;
+		grid[4][23]=1;
+		grid[5][23]=1;
+		grid[6][23]=1;
+		grid[3][24]=1;
+		grid[7][24]=1;
+		grid[3][26]=1;
+		grid[7][26]=1;
+		grid[2][26]=1;
+		grid[8][26]=1;
+		grid[4][36]=1;
+		grid[4][37]=1;
+		grid[5][36]=1;
+		grid[5][37]=1;
+		/*if(i==60 && j==60) {
 			cell[i][j].setBackground(Color.BLACK);
-		if(i==1 && j==4)
+			grid[i][j]=1;
+		}
+		if(i==60 && j==61) {
 			cell[i][j].setBackground(Color.BLACK);
-		if(i==0 && j==5)
+			grid[i][j]=1;
+		}
+		if(i==60 && j==62) {
 			cell[i][j].setBackground(Color.BLACK);
-		if(i==1 && j==5)
+			grid[i][j]=1;
+		}
+		if(i==4 && j==2) {
 			cell[i][j].setBackground(Color.BLACK);
-		if(i==12 && j==2)
+			grid[i][j]=1;
+		}
+		if(i==2 && j==2) {
 			cell[i][j].setBackground(Color.BLACK);
-		if(i==13 && j==2)
+			grid[i][j]=1;
+		}
+		if(i==3 && j==2) {
 			cell[i][j].setBackground(Color.BLACK);
-		if(i==3 && j==11)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==4 && j==10)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==5 && j==10)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==6 && j==10)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==7 && j==11)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==8 && j==12)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==8 && j==13)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==5 && j==14)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==3 && j==15)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==4 && j==16)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==5 && j==16)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==6 && j==16)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==5 && j==17)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==2 && j==20)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==3 && j==20)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==4 && j==20)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==2 && j==21)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==3 && j==21)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==4 && j==21)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==1 && j==22)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==5 && j==22)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==0 && j==24)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==1 && j==24)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==5 && j==24)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==6 && j==24)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==2 && j==34)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==3 && j==34)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==2 && j==35)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==3 && j==35)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==7 && j==15)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==60 && j==60)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==60 && j==61)
-			cell[i][j].setBackground(Color.BLACK);
-		if(i==60 && j==62)
-			cell[i][j].setBackground(Color.BLACK);
-		pan.add(cell[i][j]);
+			grid[i][j]=1;
+		}*/
 	}
 	int getN(int x,int y) {
-		int n = 0;
-
-		int startPosX = (x - 1 < 0) ? x : x-1;
-		int startPosY = (y - 1 < 0) ? y : y-1;
-		int endPosX = (x + 1 > 68) ? x : x+1;
-		int endPosY = (y + 1 > 68) ? y : y+1;
-
-		for (int i = startPosX; i<=endPosX; i++)
-			for (int j = startPosY; j<=endPosY; j++)
-				if (cell[i][j].getBackground()==Color.BLACK)
+		int n=0;
+		for(int i=-1;i<=1;i++) {
+			for(int j=-1;j<=1;j++) {
+				if(cell[x+i][y+j].getBackground().equals(Color.BLACK))
 					n++;
-		return n;
-	}
-	void loop() {
-		for(int i=0;i<69;i++) {
-			for(int j=0;j<69;j++) {
-				int neighbours=getN(i,j);
-				//System.out.println(neighbours);
-				if(neighbours<2 || neighbours>3)
-					cell[i][j].setBackground(Color.WHITE);
-				else if(neighbours==3)
-					cell[i][j].setBackground(Color.BLACK);
 			}
 		}
+		if(cell[x][y].getBackground().equals(Color.BLACK))
+			n--;
+		return n;
+	}
+	void update() {
+		for(int i=1;i<68;i++) {
+			for(int j=1;j<68;j++) {
+				if(grid[i][j]==1) cell[i][j].setBackground(Color.BLACK);
+				else cell[i][j].setBackground(Color.WHITE);
+			}
+		}
+	}
+	void loop() {
+		for(int i=1;i<68;i++) {
+			for(int j=1;j<68;j++) {
+				int neighbours=getN(i,j);
+				//System.out.print(grid[i][j]+" ");
+				if(neighbours<2 || neighbours>3)
+					grid[i][j]=0;
+				if(neighbours==3)
+					grid[i][j]=1;
+			}
+			System.out.println();
+		}
+		update();
+		//System.exit(0);
 	}
 	class Update extends Thread{
 		public void run() {
 			while(b) {
+				//b=false;
 				loop();
 				try {
 					Thread.sleep(69);
@@ -152,6 +191,6 @@ public class GameOfLife extends JFrame{
 	public static void main(String[] args){
 		frame=new GameOfLife();
 		frame.setVisible(true);
-		frame.new Update().start();
+		//frame.new Update().start();
 	}
 }
